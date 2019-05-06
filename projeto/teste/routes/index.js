c1998427a38a0273230e07d2d36164da14f44cc3
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-06 17:57:07 -0300

    at

--- projeto/teste/routes/index.js
+++ projeto/teste/routes/index.js
@@ -6,10 +6,10 @@ sql.close();
 
 
 const config = {
-  user: 'igor.silva',
-  password: '#Gf47633297824',
-  server: 'server01191034.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'BancoDigitalTime',
+  user: 'davi.teixeira',
+  password: '#Gf31772308897',
+  server: 'davi-server.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'Davi-Bandtec',
 
   options: {
       encrypt: true // Use this if you're on Windows Azure
@@ -32,7 +32,7 @@ router.get('/', function(req, res, next) {
     new sql.Request().query('select * from monitoramento', (err, result) => {
         // ... error checks
  
-        //console.dir(result)
+        console.dir(result)
         console.dir(err)
     });
   });
