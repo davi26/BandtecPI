d8cea1dd294e1d2f3e459b9e1e3035afeb9deba7
Aluno <Aluno@Digital.net>
2019-05-20 09:41:27 -0300

    acho que agora foi

---
+++ projeto/teste/configs/db.js
@@ -0,0 +1,37 @@
+const sql = require('mssql');
+const config = {
+    user: 'davi.teixeira',
+    password: '#Gf31772308897',
+    server: 'davi-server.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'Davi-Bandtec',
+  
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+  }
+module.exports={
+    connect(){
+        return new Promise((resolve,reject)=>{
+
+            sql.close();
+            sql.connect(config, err => {
+             
+                var request = new sql.Request()
+            
+                if(err){
+                    console.log(err);
+                    
+                }else{
+                    resolve(request);
+
+                }
+                
+             
+                
+            });
+        });
+        
+    }
+}
+
+
--- projeto/teste/routes/index.js
+++ projeto/teste/routes/index.js
@@ -1,52 +1,52 @@
 var express = require('express');
 var router = express.Router();
-var sql = require('mssql');
+const mssql = require('../configs/db')
 
 
 
 
-const config = {
-  user: 'davi.teixeira',
-  password: '#Gf31772308897',
-  server: 'davi-server.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'Davi-Bandtec',
 
-  options: {
-      encrypt: true // Use this if you're on Windows Azure
-  }
-}
+
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
+router.get('/cadastro', async function(req, res, next) {
+  const sql = await mssql.connect();
+
+  sql.query('select * from cadastro',(err,result)=>{
+
+    res.send(result);
+  });
   
 
+});
+
+router.get('/',  function(req, res, next) {
+ 
   res.render('cadastro');
+  
+
 });
 
 
 
-router.post('/', function(req, res, next){
 
 
-  
-  sql.connect(config, err => {
-    new sql.Request().query(`insert into cadastro values ('${req.body.nome}',
-                                                          '${req.body.email}',
-                                                          '${req.body.telefone}',
-                                                          '${req.body.cpf}',
-                                                          '${req.body.senha}')
-                                                           `, (result, err) => {
-        // ... error checks
- 
-        console.dir(result)
-        console.dir(err)
+router.post('/', async function(req, res, next){
+  const sql = await mssql.connect();
+
+  sql.query(`insert into cadastro values ('${req.body.nome}',
+      '${req.body.email}',
+      '${req.body.telefone}',
+      '${req.body.cpf}',
+      '${req.body.senha}')
+      `, (result, err) => {
+    
+
+    console.dir(result)
+    console.dir(err)
     });
-  });
   
-  sql.close(function(value) {
-    console.log("connection6 close")
-  });
+  
 });
 module.exports = router;
